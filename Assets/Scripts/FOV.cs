e055dfd92ae6d562006be89ae97bdb78b879335d
mirstigais <uprisegaming1@gmail.com>
2021-04-26 18:15:42 +0300

    Changed a lot
    Started making isometric game. Reworked controls for isometric view, basic patrol script added, not yet working FOV/LOS script.

---
+++ Assets/Scripts/FOV.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FOV : MonoBehaviour
+{
+
+    [SerializeField]
+    float fov = 90f , 
+    viewDistance = 50, 
+    angle = 0f;
+    
+    [SerializeField]
+    int rayCount = 50;
+ 
+    void Start()
+    {
+        Mesh mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        Vector3 origin = Vector3.zero;
+        float angleIncrease = fov / rayCount;
+ 
+
+        Vector3[] vertices = new Vector3[rayCount + 1 + 1];
+        Vector2[] uv = new Vector2[vertices.Length];
+        int[] triangles = new int[rayCount * 3];
+
+        vertices[0] = origin;
+
+        int vertexIndex = 1;
+        int triangleIndex = 0;
+        for (int i= 0; i <= rayCount; i++)
+        {
+            Vector3 vertex;
+            RaycastHit hit;
+            if (Physics.Raycast(origin,GetVectorFromAngle(angle),out hit,viewDistance)){
+                //hit object
+                vertex = hit.point;
+                Debug.Log("hit object");
+            }
+            else
+            {
+                //no hit
+                vertex = origin + GetVectorFromAngle(angle) * viewDistance;
+                Debug.Log("no hit");
+            }
+
+            vertices[vertexIndex] = vertex;
+
+            if (i > 0)
+            {
+                triangles[triangleIndex + 0] = 0;
+                triangles[triangleIndex + 1] = vertexIndex - 1;
+                triangles[triangleIndex + 2] = vertexIndex;
+
+                triangleIndex += 3;
+            }
+
+            vertexIndex++;
+
+            angle -= angleIncrease; //counter clockwise
+        }
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 GetVectorFromAngle(float angle)
+    {
+        //angle 0 -> 360
+        float angleRad = angle * (Mathf.PI / 180f);
+        return new Vector3(Mathf.Cos(angleRad), Mathf.Sin(angleRad));
+    }
+}
---
+++ Assets/Scripts/Patrol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] waypoints;
+
+    [SerializeField]
+    int speed;
+
+    private int waypointIndex;
+    private float dist;
+
+    void Start()
+    {
+        waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex].position);
+    }
+
+    void Update()
+    {
+        dist = Vector3.Distance(transform.position, waypoints[waypointIndex].position);
+        if (dist < 1f) IncreaseIndex();
+        PatrolAround();
+    }
+
+    void PatrolAround()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex >= waypoints.Length) waypointIndex = 0;
+        transform.LookAt(waypoints[waypointIndex]);
+    }
+}
