2cb9a714e47286f98405945fde8bd5b787a18155
mirstigais <uprisegaming1@gmail.com>
2021-04-30 16:51:56 +0300

    Finished FOV script
    FOV script now should work as intended.  Thanks to Sebastian Lague's excellent tutorial.

--- Assets/Scripts/FOV.cs
+++ Assets/Scripts/FOV.cs
@@ -16,6 +16,8 @@ public class FOV : MonoBehaviour
     public List<Transform> visibleTargets = new List<Transform>();
 
     public float meshResolution;
+    public int edgeResolveIterations;
+    public float edgeDstThreshold;
     
     public MeshFilter viewMeshFilter;
     Mesh viewMesh;
@@ -73,15 +75,35 @@ public class FOV : MonoBehaviour
         int rayCount = Mathf.RoundToInt(viewAngle * meshResolution);
         float rayAngleSize = viewAngle / rayCount;
         List<Vector3> viewPoints = new List<Vector3>();
+        ViewCastInfo oldViewCast = new ViewCastInfo();
 
         for (int i = 0; i<= rayCount; i++)
         {
             //Some trigonometry nightmare
             float angle = transform.eulerAngles.y - viewAngle / 2 + rayAngleSize * i;
             //Debug.DrawLine(transform.position, transform.position + DirFromAngle(angle, true) * viewRadius, Color.red);
-
             ViewCastInfo newViewCast = viewCast(angle);
+
+            if (i> 0)
+            {
+                bool edgeDstThresholdExceeded = Mathf.Abs(oldViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+                if (oldViewCast.hit != newViewCast.hit || (oldViewCast.hit && newViewCast.hit && edgeDstThresholdExceeded))
+                {
+                    EdgeInfo edge = FindEdge(oldViewCast, newViewCast);
+                    if (edge.pointA != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointA);
+                    }
+                    if (edge.pointB != Vector3.zero)
+                    {
+                        viewPoints.Add(edge.pointB);
+                    }
+                }
+            }
+
             viewPoints.Add(newViewCast.point);
+            oldViewCast = newViewCast;
+
         }
 
         //Setting the vertices for mesh
@@ -109,6 +131,34 @@ public class FOV : MonoBehaviour
         viewMesh.vertices = vertices;
         viewMesh.triangles = triangles;
         viewMesh.RecalculateNormals();
+    }
+
+     EdgeInfo FindEdge (ViewCastInfo minViewCast, ViewCastInfo maxViewCast)
+    {
+        float minAngle = minViewCast.angle;
+        float maxAngle = maxViewCast.angle;
+        Vector3 minPoint = Vector3.zero;
+        Vector3 maxPoint = Vector3.zero;
+
+        for (int i  = 0; i< edgeResolveIterations; i++)
+        {
+            float angle = (minAngle + maxAngle) / 2;
+            ViewCastInfo newViewCast = viewCast(angle);
+
+            bool edgeDstThresholdExceeded = Mathf.Abs(minViewCast.dst - newViewCast.dst) > edgeDstThreshold;
+
+            if (newViewCast.hit == minViewCast.hit && !edgeDstThresholdExceeded)
+            {
+                minAngle = angle;
+                minPoint = newViewCast.point;
+            }
+            else
+            {
+                maxAngle = angle;
+                maxPoint = newViewCast.point;
+            }
+        }
+        return new EdgeInfo(minPoint, maxPoint);
     }
 
     ViewCastInfo viewCast (float globalAngle)
@@ -145,6 +195,19 @@ public class FOV : MonoBehaviour
             point = _point;
             dst = _dst;
             angle = _angle;
+        }
+    }
+
+    public struct EdgeInfo
+    {
+        public Vector3 pointA;
+        public Vector3 pointB;
+
+        public EdgeInfo(Vector3 _pointA, Vector3 _pointB)
+        {
+            pointA = _pointA;
+            pointB = _pointB;
+
         }
     }
 }
